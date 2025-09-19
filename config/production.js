c7290594b21da34b5d9fbdc1256a27a93f2cef00
mitsustays <mitsustays@gmail.com>
2025-09-19 20:23:54 +0530

    Initial commit

--- config/production.js
+++ config/production.js
@@ -1,7 +1,7 @@
 module.exports = {
   database: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: process.env."postgresql://mitsustays_db_user:B66uLw6aDJPN3upLs3swmUWTJ7vVAX9l@dpg-d36mqbvdiees73bvgqr0-a.oregon-postgres.render.com/mitsustays_db",
     pool: { min: 0, max: 10 }
   }
 };
--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env."postgresql://mitsustays_db_user:B66uLw6aDJPN3upLs3swmUWTJ7vVAX9l@dpg-d36mqbvdiees73bvgqr0-a.oregon-postgres.render.com/mitsustays_db",
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
